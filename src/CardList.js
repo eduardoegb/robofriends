bf0b78262b8acf1c75a45796fd9f17544b077c7c
Eduardo Gonzalez Briceño <eduardoegb@gmail.com>
2020-02-04 12:07:15 -0300

    Static page created

---
+++ src/CardList.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <Fragment>
+      {robots.map(robot => {
+        return (
+          <Card
+            key={robot.id}
+            id={robot.id}
+            name={robot.name}
+            username={robot.username}
+            email={robot.email}
+          />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CardList;
