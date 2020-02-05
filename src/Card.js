16b483c6fe55652920ae983d60334db7d0abe4d2
Eduardo Gonzalez Briceño <eduardoegb@gmail.com>
2020-02-05 09:59:38 -0300

    Dynamic searchfield added

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ id, name, email }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="card bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img src={`https://robohash.org/${id}?size=200x200`} alt="robopic" />
       <div>
         <h2>{name}</h2>
